b2358572c97a03c35b890f7a7bc405735c95bf3c
odovgusha <dovgusha1@gmail.com>
2026-01-27 18:08:34 +0100

    added routes and data structure

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from flask import Flask
+from pickletools import read_uint1
+
+from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
+def load_blog_posts():
+    with open('blog_posts.json', 'r') as file:
+        return json.load(file)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
+def index():
+    blog_posts = load_blog_posts()
+    return render_template('index.html', posts=blog_posts)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(debug=True)
+
