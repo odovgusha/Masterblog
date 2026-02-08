d359da5f1c67edb4338a869896678d79a3e02df6
odovgusha <dovgusha1@gmail.com>
2026-02-08 20:58:55 +0100

    added add route and improved id definition formated code for better readability

--- app.py
+++ app.py
@@ -3,69 +3,90 @@ import json
 
 app = Flask(__name__)
 
-@app.route('/add', methods=['GET', 'POST'])
+
+def load_blog_posts():
+    with open("blog_posts.json", "r") as file:
+        return json.load(file)
+
+
+def save_blog_posts(posts):
+    with open("blog_posts.json", "w") as file:
+        json.dump(posts, file, indent=2)
+
+
+@app.route("/")
+def index():
+    posts = load_blog_posts()
+    return render_template("index.html", posts=posts)
+
+
+@app.route("/add", methods=["GET", "POST"])
 def add():
-
-    # If the user submitted the form
-    if request.method == 'POST':
-
+    if request.method == "POST":
 
         posts = load_blog_posts()
 
-        # Read form
-        author = request.form.get('author')
-        title = request.form.get('title')
-        content = request.form.get('content')
+        author = request.form.get("author")
+        title = request.form.get("title")
+        content = request.form.get("content")
 
+        if posts:
+            new_id = max(post["id"] for post in posts) + 1
+        else:
+            new_id = 1
 
-        new_id = len(posts) + 1
-
-        #new post
         new_post = {
             "id": new_id,
             "author": author,
             "title": title,
-            "content": content
+            "content": content,
         }
-
 
         posts.append(new_post)
         save_blog_posts(posts)
 
+        return redirect(url_for("index"))
 
-        return redirect(url_for('index'))
+    return render_template("add.html")
 
 
-    return render_template('add.html')
-
-def load_blog_posts():
-    with open('blog_posts.json', 'r') as file:
-        return json.load(file)
-
-@app.route('/delete/<int:post_id>')
+@app.route("/delete/<int:post_id>")
 def delete(post_id):
 
     posts = load_blog_posts()
-
-    posts = [post for post in posts if post['id'] != post_id]
+    posts = [post for post in posts if post["id"] != post_id]
 
     save_blog_posts(posts)
 
-    return redirect(url_for('index'))
+    return redirect(url_for("index"))
 
-@app.route('/')
-def index():
-    blog_posts = load_blog_posts()
-    return render_template('index.html', posts=blog_posts)
 
-def load_blog_posts():
-    with open('blog_posts.json', 'r') as file:
-        return json.load(file)
+@app.route("/update/<int:post_id>", methods=["GET", "POST"])
+def update(post_id):
 
-def save_blog_posts(posts):
-    with open('blog_posts.json', 'w') as file:
-        json.dump(posts, file, indent=2)
+    posts = load_blog_posts()
 
-if __name__ == '__main__':
+    post = None
+    for p in posts:
+        if p["id"] == post_id:
+            post = p
+            break
+
+    if post is None:
+        return "Post not found", 404
+
+    if request.method == "POST":
+
+        post["author"] = request.form.get("author")
+        post["title"] = request.form.get("title")
+        post["content"] = request.form.get("content")
+
+        save_blog_posts(posts)
+
+        return redirect(url_for("index"))
+
+    return render_template("update.html", post=post)
+
+
+if __name__ == "__main__":
     app.run(debug=True)
-
